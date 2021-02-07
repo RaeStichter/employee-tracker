c9622346108fa3b2ed695ebb00817ae196f1e87c
Rae Stichter <raestichter@gmail.com>
2021-02-07 00:00:46 -0500

    created initial tracker.db file along with schema.sql

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO employees (first_name, last_name)
+VALUES
+  ('Ronald', 'Firbank'),
+  ('Virginia', 'Woolf'),
+  ('Piers', 'Gaveston'),
+  ('Charles', 'LeRoi'),
+  ('Katherine', 'Mansfield'),
+  ('Dora', 'Carrington'),
+  ('Edward', 'Bellamy'),
+  ('Montague', 'Summers'),
+  ('Octavia', 'Butler'),
+  ('Unica', 'Zurn');
